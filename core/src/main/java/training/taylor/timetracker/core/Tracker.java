633f39fa43991785bf1dc893f1d34c2afb7f0d52
sumitkumar74604 <152770548+sumitkumar74604@users.noreply.github.com>
2024-01-02 22:48:26 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,4 +1,4 @@
-package training.taylor.timetracker.core;
+/*package training.taylor.timetracker.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -39,6 +39,42 @@ public class Tracker {
             // whatever
         }
 
+        return entries.get(index);
+    }
+}
+*/
+
+package training.taylor.timetracker.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import training.taylor.timetracker.core.dao.TimeEntry;
+
+import java.util.List;
+
+@Component
+public class Tracker {
+    private final List<TimeEntry> entries;
+
+    @Autowired
+    public Tracker(List<TimeEntry> entries) {
+        this.entries = entries;
+    }
+
+    public void add(TimeEntry entry) {
+        entries.add(entry);
+    }
+
+    public void remove(TimeEntry entry) {
+        entries.remove(entry);
+    }
+
+    public int size() {
+        return entries.size();
+    }
+
+    public TimeEntry get(int index) {
+        // Handle index out of bounds if necessary
         return entries.get(index);
     }
 }
